840c67850c2f7e1263c95a19341c298e6622bb6b
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2016-04-02 13:16:05 -0300

    Aula 1 - Exemplo de pilha

---
+++ Aula 1 - Pilha/Pilha/Stack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Stack
+//
+//  Created by Fabricio Serralvo on 4/2/16.
+//  Copyright © 2016 Fabricio Serralvo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Hello, World!");
+    return 0;
+}
