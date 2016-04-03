56f5765af59249558959be87e61a49c69b73d856
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2016-04-03 19:40:30 -0300

    Métodos de pilha

--- Aula 1 - Pilha/Pilha/Stack/main.c
+++ Aula 1 - Pilha/Pilha/Stack/main.c
@@ -8,8 +8,40 @@
 
 #include <stdio.h>
 
+#define kStackMaxSize 10
+
+struct stack {
+    int data[kStackMaxSize];
+    int size;
+};
+
+typedef struct stack stack;
+
+void push(stack *s, int value) {
+    
+    // É necessário verificar se a pilha não está cheia antes
+    // de fazer a inserção do novo valor
+    
+    if (s->size < kStackMaxSize) {
+        s->data[s->size++] = value;
+    } else {
+        // A pilha já esta cheia, nada para fazer aqui
+    }
+}
+
+void pop(stack *s) {
+    
+    // Antes de remover um item da pilha é necessário checar
+    // se a mesma possui algum item
+    
+    if (s->size == 0) {
+        // Pilha vazia, nada para fazer aqui
+    } else {
+        s->size--;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
-    printf("Hello, World!");
     return 0;
 }
